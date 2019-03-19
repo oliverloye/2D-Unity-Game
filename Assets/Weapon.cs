c670e5b4e56077a3ec3fe15fea6c09185a9e26d7
Oliver Max Løye <oliver@Olivers-iMac.local>
2019-03-19 13:06:20 +0100

    weapon script

---
+++ Assets/Weapon.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Transform firepoint;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
